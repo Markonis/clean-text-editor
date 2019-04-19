77ff270b9f02d9712c10f4593307977410502433
Marko Pavlović <marko@affine.tech>
2019-04-19 17:29:05 +0200

    Add exports to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+import { EditorData, EditorOptions } from './editor/data-model';
 import Editor from './editor/editor';
+import render from './editor/render';
+
+export { EditorData, EditorOptions, render };
 
 export default Editor;
